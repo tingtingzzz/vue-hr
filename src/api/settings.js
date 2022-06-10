b2539f5b33eddd55622f87d4bd7a0dbd6bfbdd11
牛爱花 <65259423@qq.com>
2022-06-10 21:27:26 +0800

    在表格中显示数据

---
+++ src/api/settings.js
@@ -0,0 +1,15 @@
+import request from '@/utils/request'
+
+// 根据业务要求- 查接口文档-封装api
+/**
+ * 获取所有角色信息
+ * @param {*} params  {page, pagesize}
+ * @returns
+ */
+export function getRoles(params) {
+  return request({
+    url: '/sys/role',
+    method: 'GET',
+    'params': params
+  })
+}
--- src/views/settings/settings.vue
+++ src/views/settings/settings.vue
@@ -1,10 +1,72 @@
 <template>
-  <div class="department-container">
+  <div class="dashboard-container">
     <div class="app-container">
-        <el-card>
-          <!-- 具体页面结构 -->
-          组织架构
-        </el-card>
+      <el-card>
+        <el-tabs>
+          <!-- 放置页签 -->
+          <el-tab-pane label="角色管理">
+            <!-- 新增角色按钮 -->
+            <el-row style="height:60px">
+              <el-button
+                icon="el-icon-plus"
+                size="small"
+                type="primary"
+              >新增角色</el-button>
+            </el-row>
+            <!-- 表格 -->
+            <el-table :data="roles">
+              <el-table-column label="序号" width="120" type="index" />
+              <el-table-column label="角色名称" width="240" prop="name" />
+              <el-table-column label="描述" prop="description" />
+              <el-table-column label="操作">
+                <template slot-scope="scope">
+                  <el-button size="small" type="success">分配权限</el-button>
+                  <el-button size="small" type="primary">编辑</el-button>
+                  <el-button size="small" type="danger">删除</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+
+            <el-row type="flex" justify="center" align="middle" style="height: 60px">
+              <!-- 分页组件 -->
+              <el-pagination layout="prev,pager,next" />
+            </el-row>
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
     </div>
   </div>
 </template>
+
+<script>
+
+import { getRoles } from '@/api/settings'
+
+export default {
+  data() {
+    return {
+      roles: [], // 数据
+      total: 0 // 总条数
+    }
+  },
+  created() {
+    this.logadRoles()
+  },
+  methods: {
+    async logadRoles() {
+      try {
+        const { data: res } = await getRoles({ page: 1, pagesize: 10 })
+        this.roles = res.rows
+        this.total = res.total
+        console.log(res)
+      } catch (e) {
+        this.$message.error(e)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
